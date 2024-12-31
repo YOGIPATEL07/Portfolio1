5a3458ce1958e8290f36cd844bd66081a3b86893
YOGIPATEL07 <122917701+YOGIPATEL07@users.noreply.github.com>
2024-12-30 22:16:00 -0500

    dark mode done

--- public/script.js
+++ public/script.js
@@ -86,7 +86,7 @@ menuToggle.addEventListener('click', () => {
 
     // GSAP Scroll Animations
     gsap.from(".hero-text", { opacity: 0, y: -50, duration: 1 });
-    // gsap.from(".skills-container", { opacity: 0, y: 50, duration: 1, delay: 0.5 });
+    gsap.from(".skills-container", { opacity: 0, y: 50, duration: 1, delay: 0.5 });
     gsap.from(".project-grid", { opacity: 0, x: -100, duration: 1, delay: 1 });
 
     // Hover effect for navigation links
